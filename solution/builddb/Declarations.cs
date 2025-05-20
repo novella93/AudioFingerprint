a25e75571d3ce2d7653c55b99c587c48954ba39d
jnovella <javier.novella.ruiz.games@gmail.com>
2025-05-20 22:48:26 +0200

    ### Added - Get Spectrograms logic

--- solution/builddb/Declarations.cs
+++ solution/builddb/Declarations.cs
@@ -3,8 +3,13 @@ namespace Declarations
 {
     public class BuildDB
     {
+        
         public class Audio
         {
+            public const string MONO_FILES_FOLDER_NAME = "mono";
+            public const string SPECTROGRAM_FILES_FOLDER_NAME = "spectrograms";
+            public const int SPECTROGRAM_WINDOWS_SIZE = 8192;
+            public const int SPECTROGRAM_HOP_SIZE = 4096;
             public enum Channels
             {
                 MONO = 1,
@@ -22,6 +27,9 @@ namespace Declarations
 
         public class Program
         {
+            public const string RESULTS_FOLDER_NAME = "results";
+            public const string INPUT_PARAMETER_TOKEN = "-i";
+            public const string OUTPUT_PARAMETER_TOKEN = "-o";
             public enum State
             {
                 CHECK_INPUT_PARAMETERS,
@@ -32,6 +40,16 @@ namespace Declarations
                 GET_SPECTROGRAMS,
                 SUCCESS,
                 ERROR
+            }
+
+            public class ErrorTexts
+            {
+                public const string ERROR_INPUT_PATH_DOES_NOT_EXIST = "Input path does not exist.";
+                public const string ERROR_INVALID_OUTPUT_PATH = "Invalid output path.";
+                public const string ERROR_INVALID_NUMBER_OF_ARGUMENTS = "Invalid number of arguments provided.";
+                public const string ERROR_INVALID_ARGUMENTS = "Check parameters provided.";
+                public const string ERROR_NO_AUDIO_FILES_FOUND = "No files found in the provided song folder.";
+
             }
         }
     }
--- solution/builddb/Program.cs
+++ solution/builddb/Program.cs
@@ -1,12 +1,19 @@
 ﻿using Declarations;
+using System.Text.Json;
+using System.IO;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
+using NWaves.Audio;
+using NWaves.Transforms;
+using NWaves.Windows;
+
 class Program
 {
     // State of the program
     static BuildDB.Program.State State = BuildDB.Program.State.CHECK_INPUT_PARAMETERS;
     // Audio files handling
     static BuildDB.Audio.Files Files = new BuildDB.Audio.Files();
+    static List<float[]>[] stftResults = new List<float[]>[0];
     static void Main(string[] args)
     {
         bool execute = true;
@@ -18,7 +25,7 @@ class Program
                     if (args.Length == 4)
                     {
                         int validArguments = 0;
-                        if (args[0] == "-i")
+                        if (args[0] == BuildDB.Program.INPUT_PARAMETER_TOKEN)
                         {
                             validArguments++;
                             if (Directory.Exists(args[1]))
@@ -27,11 +34,11 @@ class Program
                             }
                             else
                             {
-                                Console.WriteLine("Input path does not exist.");
+                                Console.WriteLine(BuildDB.Program.ErrorTexts.ERROR_INPUT_PATH_DOES_NOT_EXIST);
                                 State = BuildDB.Program.State.ERROR;
                             }
                         }
-                        if (args[2] == "-o")
+                        if (args[2] == BuildDB.Program.OUTPUT_PARAMETER_TOKEN)
                         {
                             validArguments++;
                             if (args[3] != "")
@@ -41,7 +48,7 @@ class Program
                             }
                             else
                             {
-                                Console.WriteLine("Wrong output path.");
+                                Console.WriteLine(BuildDB.Program.ErrorTexts.ERROR_INVALID_OUTPUT_PATH);
                                 State = BuildDB.Program.State.ERROR;
                             }
                         }
@@ -51,13 +58,13 @@ class Program
                         }
                         else
                         {
-                            Console.WriteLine("Check parameters provided.");
+                            Console.WriteLine(BuildDB.Program.ErrorTexts.ERROR_INVALID_ARGUMENTS);
                             State = BuildDB.Program.State.ERROR;
                         }
                     }
                     else
                     {
-                        Console.WriteLine("Invalid number of arguments provided.");
+                        Console.WriteLine(BuildDB.Program.ErrorTexts.ERROR_INVALID_NUMBER_OF_ARGUMENTS);
                         State = BuildDB.Program.State.ERROR;
                     }
                     break;
@@ -73,19 +80,23 @@ class Program
                     }
                     else
                     {
-                        Console.WriteLine("No files found in the provided song folder.");
+                        Console.WriteLine(BuildDB.Program.ErrorTexts.ERROR_NO_AUDIO_FILES_FOUND);
                         State = BuildDB.Program.State.ERROR;
                     }
                     break;
 
                 case BuildDB.Program.State.CREATE_FOLDERS:
-                    if (!Directory.Exists("results"))
+                    if (!Directory.Exists(BuildDB.Program.RESULTS_FOLDER_NAME))
                     {
-                        Directory.CreateDirectory("results");
+                        Directory.CreateDirectory(BuildDB.Program.RESULTS_FOLDER_NAME);
                     }
-                    if (!Directory.Exists("results\\mono"))
+                    if (!Directory.Exists(BuildDB.Program.RESULTS_FOLDER_NAME + "\\" + BuildDB.Audio.MONO_FILES_FOLDER_NAME))
                     {
-                        Directory.CreateDirectory("results\\mono");
+                        Directory.CreateDirectory(BuildDB.Program.RESULTS_FOLDER_NAME + "\\" + BuildDB.Audio.MONO_FILES_FOLDER_NAME);
+                    }
+                    if (!Directory.Exists(BuildDB.Program.RESULTS_FOLDER_NAME + "\\" + BuildDB.Audio.SPECTROGRAM_FILES_FOLDER_NAME))
+                    {
+                        Directory.CreateDirectory(BuildDB.Program.RESULTS_FOLDER_NAME + "\\" + BuildDB.Audio.SPECTROGRAM_FILES_FOLDER_NAME);
                     }
                     State = BuildDB.Program.State.CONVERT_SONGS_TO_MONO;
                     break;
@@ -93,18 +104,45 @@ class Program
                 case BuildDB.Program.State.CONVERT_SONGS_TO_MONO:
                     for (int idx = 0; idx < Files.OriginalsPath.Length; idx++)
                     {
-                        using var audio = new AudioFileReader(Files.OriginalsPath[idx]);
-                        if (audio.WaveFormat.Channels == (int)BuildDB.Audio.Channels.STEREO)
+                        if (!File.Exists(BuildDB.Program.RESULTS_FOLDER_NAME + "\\" + BuildDB.Audio.MONO_FILES_FOLDER_NAME + "\\" + Files.OriginalsName[idx]))
                         {
-                            var monoProvider = new StereoToMonoSampleProvider(audio) { LeftVolume = 0.5f, RightVolume = 0.5f };
-                            WaveFileWriter.CreateWaveFile16("results\\mono\\" + Files.OriginalsName[idx], monoProvider);
-                        }
-                        else if (audio.WaveFormat.Channels == (int)BuildDB.Audio.Channels.MONO)
-                        {
-                            File.Copy(Files.OriginalsPath[idx], "results\\mono\\" + Files.OriginalsName[idx]);
+                            using var audio = new AudioFileReader(Files.OriginalsPath[idx]);
+                            if (audio.WaveFormat.Channels == (int)BuildDB.Audio.Channels.STEREO)
+                            {
+                                var monoProvider = new StereoToMonoSampleProvider(audio) { LeftVolume = 0.5f, RightVolume = 0.5f };
+                                WaveFileWriter.CreateWaveFile16(BuildDB.Program.RESULTS_FOLDER_NAME + "\\" + BuildDB.Audio.MONO_FILES_FOLDER_NAME + "\\" + Files.OriginalsName[idx], monoProvider);
+                            }
+                            else if (audio.WaveFormat.Channels == (int)BuildDB.Audio.Channels.MONO)
+                            {
+                                File.Copy(Files.OriginalsPath[idx], BuildDB.Program.RESULTS_FOLDER_NAME + "\\" + BuildDB.Audio.MONO_FILES_FOLDER_NAME + "\\" + Files.OriginalsName[idx]);
+                            }
                         }
                         Files.MonoName[idx] = Files.OriginalsName[idx];
-                        Files.MonoPath[idx] = "results\\mono\\" + Files.OriginalsName[idx];
+                        Files.MonoPath[idx] = BuildDB.Program.RESULTS_FOLDER_NAME + "\\" + BuildDB.Audio.MONO_FILES_FOLDER_NAME + "\\" + Files.OriginalsName[idx];
+                    }
+                    State = BuildDB.Program.State.GET_SPECTROGRAMS;
+                    break;
+
+                case BuildDB.Program.State.GET_SPECTROGRAMS:
+                    Array.Resize(ref stftResults, Files.MonoPath.Length);
+                    for (int idx = 0; idx < Files.MonoPath.Length; idx++)
+                    {
+                        // if (!File.Exists(BuildDB.Program.RESULTS_FOLDER_NAME + "\\" + BuildDB.Audio.SPECTROGRAM_FILES_FOLDER_NAME + "\\" + Files.MonoName[idx] + ".json"))
+                        // {
+                            var fileStream = File.OpenRead(Files.MonoPath[idx]);
+                            var audio = new WaveFile(fileStream);
+                            var signal = audio[Channels.Left];
+                            int windowSize = BuildDB.Audio.SPECTROGRAM_WINDOWS_SIZE;
+                            int hopSize = BuildDB.Audio.SPECTROGRAM_HOP_SIZE;
+                            var stft = new Stft(windowSize, hopSize, WindowType.Hann);
+                            stftResults[idx] = stft.Spectrogram(signal);
+                            // string json = JsonSerializer.Serialize(stftResults);
+                            // File.WriteAllText(BuildDB.Program.RESULTS_FOLDER_NAME + "\\" + BuildDB.Audio.SPECTROGRAM_FILES_FOLDER_NAME + "\\" + Files.MonoName[idx] + ".json", json);
+                        // } else
+                        // {
+                        //     stftResults[idx] = JsonSerializer.Deserialize<List<float[]>>(File.ReadAllText(BuildDB.Program.RESULTS_FOLDER_NAME + "\\" + BuildDB.Audio.SPECTROGRAM_FILES_FOLDER_NAME + "\\" + Files.MonoName[idx] + ".json"));
+                        // }
+
                     }
                     State = BuildDB.Program.State.SUCCESS;
                     break;
